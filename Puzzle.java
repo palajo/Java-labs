8ff46e76211118add9c06ff3f3700916e948669b
Oleh <Palaj@MacBook-Pro-Mac.local>
2020-02-17 00:41:43 +0200

    Lab1

---
+++ Puzzle.java
@@ -0,0 +1,125 @@
+package ua.lviv.iot.first.lab;
+
+/* Поля з таблички:
+	- опис пазлу
+	- кількість елементів пазлу
+	- ширина коробки
+	- висота коробки
+*/
+public class Puzzle {
+
+	/* Поля з таблички */
+	private String description;
+	private int quanitity;
+	private int widthofbox;
+	private int heightofbox;
+
+	/* 2 додаткових приватних поля */
+	private String sex;
+	private String theme;
+
+	/* 1 статичне числового типу */
+	public static int price = 9;
+
+	/* 2 захищених поля */
+	protected String vendor;
+	protected int guarantee;
+
+	/* Конструктор */
+	public Puzzle(String description, int quanitity, int widthofbox, int heightofbox, String sex, String theme,
+			String vendor, int guarantee) {
+		this.description = description;
+		this.quanitity = quanitity;
+		this.widthofbox = widthofbox;
+		this.heightofbox = heightofbox;
+		this.sex = sex;
+		this.theme = theme;
+		this.vendor = vendor;
+		this.guarantee = guarantee;
+	}
+
+	/* Конструктор з першими 4 полями */
+	public Puzzle(String description, int quanitity, int widthofbox, int heightofbox) {
+		this(description, quanitity, widthofbox, heightofbox, null, null, null, 0);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getQuanitity() {
+		return quanitity;
+	}
+
+	public void setQuanitity(int quanitity) {
+		this.quanitity = quanitity;
+	}
+
+	public int getWidthofbox() {
+		return widthofbox;
+	}
+
+	public void setWidthofbox(int widthofbox) {
+		this.widthofbox = widthofbox;
+	}
+
+	public int getHeightofbox() {
+		return heightofbox;
+	}
+
+	public void setHeightofbox(int heightofbox) {
+		this.heightofbox = heightofbox;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	public static int getPrice() {
+		return price;
+	}
+
+	public static void setPrice(int price) {
+		Puzzle.price = price;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public int getGuarantee() {
+		return guarantee;
+	}
+
+	public void setGuarantee(int guarantee) {
+		this.guarantee = guarantee;
+	}
+
+	@Override
+	public String toString() {
+		return "Puzzle [description=" + description + ", quanitity=" + quanitity + ", widthofbox=" + widthofbox
+				+ ", heightofbox=" + heightofbox + ", sex=" + sex + ", theme=" + theme + ", vendor=" + vendor
+				+ ", guarantee=" + guarantee + "]";
+	}
+
+}
---
+++ PuzzleMaker.java
@@ -0,0 +1,12 @@
+package ua.lviv.iot.first.lab;
+
+public class PuzzleMaker {
+
+	public static void main(String[] args) {
+		
+		Puzzle puzzle = new Puzzle("description of puzzle", 100, 75, 25); 	
+		
+		System.out.println(puzzle);
+	}
+
+}
